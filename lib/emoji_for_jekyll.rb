f612288cc5769ce98d689bd70f6c0b83eb8d1dca
Yihang Ho <hoyihang5@gmail.com>
2014-05-08 10:15:12 +0800

    Convert all tabs to spaces.

--- lib/emoji_for_jekyll.rb
+++ lib/emoji_for_jekyll.rb
@@ -2,59 +2,59 @@ require 'json'
 
 module EmojiForJekyll
 
-	class EmojiGenerator < Jekyll::Generator
-		def generate(site)
-			if site.config.has_key?("emoji") and !site.config["emoji"]
-				return
-			end
+  class EmojiGenerator < Jekyll::Generator
+    def generate(site)
+      if site.config.has_key?("emoji") and !site.config["emoji"]
+        return
+      end
 
-			if site.config.has_key?("emoji-additional-keys")
-				additional_keys = site.config["emoji-additional-keys"]
-			else
-				additional_keys = []
-			end
+      if site.config.has_key?("emoji-additional-keys")
+        additional_keys = site.config["emoji-additional-keys"]
+      else
+        additional_keys = []
+      end
 
-			get_master_whitelist
+      get_master_whitelist
 
-			get_images_path(site)
+      get_images_path(site)
 
-			site.pages.each { |p| substitute(p, additional_keys) }
+      site.pages.each { |p| substitute(p, additional_keys) }
 
-			site.posts.each { |p| substitute(p, additional_keys) }
-		end
+      site.posts.each { |p| substitute(p, additional_keys) }
+    end
 
-		private
-		def get_master_whitelist
-			# @master_whitelist is an array of all supported emojis
-			@master_whitelist = JSON.parse(IO.readlines(File.expand_path("emoji.json", File.dirname(__FILE__))).join)
-		end
+    private
+    def get_master_whitelist
+      # @master_whitelist is an array of all supported emojis
+      @master_whitelist = JSON.parse(IO.readlines(File.expand_path("emoji.json", File.dirname(__FILE__))).join)
+    end
 
-		def get_images_path(site)
-			@images_path = {}
-			if site.config["emoji-images-path"]
+    def get_images_path(site)
+      @images_path = {}
+      if site.config["emoji-images-path"]
 
-			  images_path = site.config["emoji-images-path"]
-			  Dir.foreach(images_path) do |image_filename|
-			    if /^(?<tag>.*)\.(?:png|jpg|jpeg|gif)/ =~ image_filename
-			      @master_whitelist << tag
-			      @images_path[tag] = File.join(images_path, image_filename)
-			    end
-			  end
+        images_path = site.config["emoji-images-path"]
+        Dir.foreach(images_path) do |image_filename|
+          if /^(?<tag>.*)\.(?:png|jpg|jpeg|gif)/ =~ image_filename
+            @master_whitelist << tag
+            @images_path[tag] = File.join(images_path, image_filename)
+          end
+        end
 
-			end
-			@master_whitelist.sort!
-		end
+      end
+      @master_whitelist.sort!
+    end
 
-		def substitute(obj, additional_keys)
-			if obj.data.has_key?("emoji") and !obj.data["emoji"]
-				return
-			end
+    def substitute(obj, additional_keys)
+      if obj.data.has_key?("emoji") and !obj.data["emoji"]
+        return
+      end
 
-			whitelist = obj.data.has_key?("emoji-whitelist") ? obj.data["emoji-whitelist"] : false
-			blacklist = obj.data.has_key?("emoji-blacklist") ? obj.data["emoji-blacklist"] : false
+      whitelist = obj.data.has_key?("emoji-whitelist") ? obj.data["emoji-whitelist"] : false
+      blacklist = obj.data.has_key?("emoji-blacklist") ? obj.data["emoji-blacklist"] : false
 
-			# When both the whitelist and blacklist are defined, whitelist will be prioritized
-			blacklist = whitelist ? false : blacklist
+      # When both the whitelist and blacklist are defined, whitelist will be prioritized
+      blacklist = whitelist ? false : blacklist
 
       filter = Proc.new do |key|
         (whitelist and whitelist.include?($1)) or
@@ -63,18 +63,18 @@ module EmojiForJekyll
         @master_whitelist.bsearch { |i| i >= key } == key
       end
 
-			obj.content.gsub!(/:([\w\+\-]+):/) do |s|
-				convert($1, filter)
-			end
+      obj.content.gsub!(/:([\w\+\-]+):/) do |s|
+        convert($1, filter)
+      end
 
-			additional_keys.each do |key|
-				if obj.data.has_key?(key)
-					obj.data[key].gsub!(/:([\w\+\-]+):/) do |s|
-						convert($1, filter)
-					end
-				end
-			end
-		end
+      additional_keys.each do |key|
+        if obj.data.has_key?(key)
+          obj.data[key].gsub!(/:([\w\+\-]+):/) do |s|
+            convert($1, filter)
+          end
+        end
+      end
+    end
 
     # convert takes in the key to the emoji to be converted and an optional block
     # If block is provided, conversion will be done only if this block evaluates to true.
@@ -86,15 +86,15 @@ module EmojiForJekyll
       end
     end
 
-		def img_tag(name)
-			# if there is an image in the custom images path
-			if @images_path[name]
-				img_src = '/'+@images_path[name]
-			else # otherwise use fallback CDN
-				img_src = "https://github.global.ssl.fastly.net/images/icons/emoji/#{name}.png"
-			end
-			
-			"<img class='emoji' title='#{name}' alt='#{name}' src='#{img_src}' height='20' width='20' align='absmiddle' >"
-		end
-	end
+    def img_tag(name)
+      # if there is an image in the custom images path
+      if @images_path[name]
+        img_src = '/'+@images_path[name]
+      else # otherwise use fallback CDN
+        img_src = "https://github.global.ssl.fastly.net/images/icons/emoji/#{name}.png"
+      end
+
+      "<img class='emoji' title='#{name}' alt='#{name}' src='#{img_src}' height='20' width='20' align='absmiddle' >"
+    end
+  end
 end
--- update-json.rb
+++ update-json.rb
@@ -6,10 +6,10 @@ require 'json'
 uri = URI('https://api.github.com/emojis')
 
 Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
-	request = Net::HTTP::Get.new uri
-	response = http.request request
-	json_obj = JSON.parse response.body
-	File.open("lib/emoji.json", "w") do |f|
-		f.syswrite json_obj.keys
-	end
+  request = Net::HTTP::Get.new uri
+  response = http.request request
+  json_obj = JSON.parse response.body
+  File.open("lib/emoji.json", "w") do |f|
+    f.syswrite json_obj.keys
+  end
 end
